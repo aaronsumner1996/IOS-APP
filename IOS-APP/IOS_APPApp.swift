77b53da4c07ca08bf9b4ab6a0f425433ddb167bf
aaronsumner1996 <aaronsumner1996@googlemail.com>
2023-05-22 23:08:26 +0100

    Initial Commit

---
+++ IOS-APP/IOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_APPApp.swift
+//  IOS-APP
+//
+//  Created by Sumner, Aaron on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct IOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
